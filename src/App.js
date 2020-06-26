f919b4879858d44f8279f83f1c83052824b923ec
Omama Zainab <omi.zainy@gmail.com>
2020-06-26 20:56:33 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [repo,setRepo] = useState([{}]);
+
+  useEffect(() => {
+
+    async function fetchRepos(){
+
+      const response = await fetch('https://api.github.com/users/omamazainab/repos'); 
+      const data = await response.json();
+      setRepo(data);
+      
+    }
+    fetchRepos();
+
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <ul>
+        {
+          repo.map( (repoObj) => {
+            return (<li>{repoObj.name}</li>)
+          })
+        }
+      </ul>
     </div>
   );
 }
